d68b624df6d3a0c6b84aa2ce460a14778cb611cd
Aaron <aagavin@gmail.com>
2017-04-01 09:36:14 -0400

    pull chagnes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -30,7 +30,6 @@ module.exports.DisplayLogin = (req, res) => {
 }
 
 // Processes the Login Request
-//module.exports.ProcessLogin = (req, res, next) => {
 module.exports.ProcessLogin = (req, res) => {
   firebaseAuth.signInWithEmailAndPassword(req.body.email, req.body.password)
   .then(()=> {
