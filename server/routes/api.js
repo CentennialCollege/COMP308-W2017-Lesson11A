3f8de3b7f1498295da8f2d9d6e583b3f221d9975
Arlina Ramrattan <arlina.ramrattan@gmail.com>
2017-04-01 09:46:21 -0400

    closes #29

--- server/routes/api.js
+++ server/routes/api.js
@@ -31,7 +31,7 @@ router.get('/games/add', usersController.RequireAuth, (req, res, next) => {
 // GET the Game Details page in order to edit a new Game
 router.get('/games/:id', usersController.RequireAuth, (req, res, next) => {
   apiController.GetGameById(req, res);
-}).post('/games/:id', usersController.RequireAuth, (req, res, next) => {
+}).post('/games/:id', usersController.RequireAuth, (req, res) => {
   // POST - process the information passed from the details form and update the document
   apiController.UpdateGame(req, res);
 });
