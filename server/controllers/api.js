433649443fe74a460bca91446ffc99025ae0dac0
Aaron <aagavin@gmail.com>
2017-04-01 09:58:06 -0400

    more commented out code

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -6,7 +6,7 @@ let game = require('../models/games');
 // required for firebase
 let firebase = require('../config/firebase');
 // let firebaseDB = firebase.games;
-let firebaseAdmin = firebase.admin;
+// let firebaseAdmin = firebase.admin;
 let firebaseAuth = firebase.auth;
 
 // Read and display the Game List
